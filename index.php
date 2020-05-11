38fc90c1fe7c3603a708c5b3ab3bdadbf1b3789e
murielledouay <guyard_murielle@orange.fr>
2020-05-11 14:22:17 +0200

    PHP Partie 4 exo 2

--- index.php
+++ index.php
@@ -1,34 +1,23 @@
 <?php
-//déclaration des 2 variables avec les valeur
-$object = 'Réunion';
-$message = 'Bonjour, nous allons faire le point sur les chiffres en cours.';
+// on déclare notre fonction
+function text($phrase,$element)
+{
+// on retourne le paramètre
+  return $phrase . ' ' . $element;
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-
 <head>
   <meta charset="utf-8"/>
   <link rel="stylesheet" href="assets/css/style.css"/>
-  <title>Partie 1 exo 1</title>
+  <title>Partie 4 exo 2</title>
 </head>
-
 <body>
-
-  <p>Objet :
-    <?php
-    //j'affiche avec echo la variable sur mon navigateur de l'objet
-    echo $object;
-    ?>
-  </p>
-
-
-  <p>
-    <?php
-      //j'affiche avec echo la variable du message sur mon navigateur
-    echo $message;
-    ?>
-  </p>
-
-</body>
+  <?php
+// si on affiche le paramètre
+// echo de la variable
+  $word = text('Ceci est une chaîne de caractères.','Ceci est une seconde chaîne de caractères'); ?>
+  <p> <?php echo $word;?> </p>
+  </body>
 </html>
